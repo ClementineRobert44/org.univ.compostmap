e193533eb8d925d9b4aab429f43199fe430ed3f7
Clementine <clementine.robert.nantes@gmail.com>
2021-11-15 16:10:16 +0100

    Exo 6

--- compostmap/src/components/Composter.vue
+++ compostmap/src/components/Composter.vue
@@ -2,15 +2,19 @@
   <h1>Compost {{ id }}</h1>
   <a v-bind:href="url+adresse">Adresse: {{ adresse }}</a>
   <div class="compost-image">
-    <img v-bind:src="image" />
+    <img v-bind:src="image" v-on:mouseover="deleteLastSchedule"/>
   </div>
 
   <div v-if="isOpen">Ouvert</div>
   <div v-else>Fermé</div>
 
   <ul> 
-    <li v-for="horaire in openSchedules" v-bind:key="horaire.day">Le {{ horaire.day }} : De {{ horaire.opening_hour }}h à {{ horaire.closing_hour }}h</li>
+    <li v-for="horaire in openSchedules" v-bind:key="horaire.day" v-on:mouseover="getDay(horaire.day)">Le {{ horaire.day }} : De {{ horaire.opening_hour }}h à {{ horaire.closing_hour }}h</li>
   </ul>
+
+  <div>{{ focusedDay }}</div>
+
+  <button v-on:click="createNewSchedule">Ajouter un jour</button>
 
   
 </template>
@@ -31,8 +35,24 @@ export default {
         { day: 'Mercredi', opening_hour: 10, closing_hour: 14},
         { day: 'Jeudi', opening_hour: 10, closing_hour: 14},
         { day: 'Vendredi', opening_hour: 9, closing_hour: 12}
-      ]
+      ],
+      focusedDay: String
     };
   },
+  methods: {
+    createNewSchedule() {
+      this.openSchedules.push({
+        day: 'Samedi', opening_hour: 9, closing_hour: 12
+      })
+    },
+
+    deleteLastSchedule() {
+      this.openSchedules.pop();
+    },
+
+    getDay(day){
+      this.focusedDay = day;
+    }
+  }
 };
 </script>
