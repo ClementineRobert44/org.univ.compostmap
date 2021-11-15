d44b3c48fd72ff9934e332dd95028f4b2619ff5b
Clementine <clementine.robert.nantes@gmail.com>
2021-11-15 15:35:39 +0100

    Exo 5

--- compostmap/src/components/Composter.vue
+++ compostmap/src/components/Composter.vue
@@ -4,6 +4,15 @@
   <div class="compost-image">
     <img v-bind:src="image" />
   </div>
+
+  <div v-if="isOpen">Ouvert</div>
+  <div v-else>Fermé</div>
+
+  <ul> 
+    <li v-for="horaire in openSchedules" v-bind:key="horaire.day">Le {{ horaire.day }} : De {{ horaire.opening_hour }}h à {{ horaire.closing_hour }}h</li>
+  </ul>
+
+  
 </template>
 
 <script>
@@ -14,7 +23,15 @@ export default {
       id: 42,
       adresse: "28 rue des plantes en pots",
       image: "./assets/img/compost1.png",
-      url: "https://www.google.com/maps/search/"
+      url: "https://www.google.com/maps/search/",
+      isOpen: false,
+      openSchedules : [       
+        { day: 'Lundi', opening_hour: 9, closing_hour: 12},
+        { day: 'Mardi', opening_hour: 10, closing_hour: 14},
+        { day: 'Mercredi', opening_hour: 10, closing_hour: 14},
+        { day: 'Jeudi', opening_hour: 10, closing_hour: 14},
+        { day: 'Vendredi', opening_hour: 9, closing_hour: 12}
+      ]
     };
   },
 };
