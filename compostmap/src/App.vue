d3e554cd85a554fb7c75ad73ef2e877cf0230a2c
Clementine <clementine.robert.nantes@gmail.com>
2021-12-13 15:27:22 +0100

    Exercice 13

--- compostmap/src/App.vue
+++ compostmap/src/App.vue
@@ -1,48 +1,30 @@
 <template>
-  <MenuView />
   <div>
-    <p class="columns">
-      <button
-        class="button is-primary"
-        @click="composters.push({ id: nextComposterId++ })"
-      >
-        Add Composter {{ nextComposterId }}
-      </button>
-      <button
-        class="button is-danger is-quarter"
-        @click="composters.splice(0, 1)"
-        v-if="composters.length > 0"
-      >
-        Remove composter {{ composters[0].id }}
-      </button>
-    </p>
-    <div class="columns is-multiline">
-      <Composter
-        v-for="composter in composters"
-        :key="composter.id"
-        class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
-        :composter-id="composter.id"
-      />
+    <!-- MenuView -->
+    <MenuView />
+
+    <!-- Content -->
+    <div class="container">
+      <!-- About Page -->
+      <AboutPage message="à propos" />
+
+      <!-- Composters list -->
+      <CompostersList />
     </div>
   </div>
 </template>
 
 <script>
-import Composter from "./components/Composter.vue";
-
+import CompostersList from "./components/CompostersList.vue";
 import MenuView from "./components/MenuView.vue";
+import AboutPage from "./components/AboutPage.vue";
 
 export default {
   name: "App",
   components: {
-    Composter,
     MenuView,
-  },
-  data() {
-    return {
-      nextComposterId: 1,
-      composters: new Array(),
-    };
+    CompostersList,
+    AboutPage,
   },
 };
 </script>
@@ -54,6 +36,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 5px;
 }
 </style>
